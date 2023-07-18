45c98846c75e85fdee3fb430e23650f13ec04550
lumutek <62620503+lumutek@users.noreply.github.com>
2023-07-18 02:37:32 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,36 @@
+// Create web server
+// var http = require('http');
+// var server = http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('Hello World!');
+// });
+// server.listen(8080);
+// console.log('Server is running on port 8080...');
+// Create web server
+// var http = require('http');
+// var server = http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+
+//     // Create object
+//     var member = {
+//         name: "John",
+//         age: 25,
+//         skills: ["HTML", "CSS", "JS", "PHP", "MySQL"]
+//     };
+
+//     // Convert object to string
+//     var memberToString = JSON.stringify(member);
+
+//     // Convert string to object
+//     var stringToMember = JSON.parse(memberToString);
+
+//     // Display
+//     res.write(stringToMember.name + "<br>");
+//     res.write(stringToMember.age + "<br>");
+//     res.write(stringToMember.skills[2]);
+//     res.end();
+// });
+// server.listen(8080);
+// console.log('Server is running on port 8080...');
+
+// Path: comments.js
